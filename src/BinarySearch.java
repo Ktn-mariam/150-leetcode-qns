3467c3b6fa244de71ed0324ffa6abacff659c41c
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-08-28 22:24:17 +0530

    Day-1-binary-search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,4 +1,20 @@
+/*
+    THEORY
+    Works only if the array is sorted !!
+    1-Initialize high as array.length -1 and low as 0
+    2-Run a while loop as long as high >= low
+    2-Find the middle index => (high + low)/2
+    3-Check if array[mid] = target
+    4-If not, check if array[mid] > target
+        => target is in the lower half of the array
+        => make high = low - 1
+    5-Else low = mid + 1
+    6-Repeat while loop until you find target
+    7-Else return -1
+ */
+
 public class BinarySearch {
+    // Iterative method
     public int search(int[] nums, int target) {
         int low = 0, high = nums.length-1;
 
@@ -15,6 +31,7 @@ public class BinarySearch {
         return -1;
     }
 
+    // Recursive Method
     public int recursiveSearch (int[] nums, int low, int high, int target){
         int mid = high + low /2;
         if (high >= low) {
