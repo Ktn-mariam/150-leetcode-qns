abc4ef079db16d5790ff43b48546bdcafa0216f6
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-08-30 17:43:01 +0530

    Day-3-valid-anagram

---
+++ src/ValidAnagram.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class ValidAnagram {
+    public boolean sorting(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+
+    public boolean hashMap(String s, String t) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (char x : s.toCharArray()) {
+            hm.put(x, hm.getOrDefault(x, 0) + 1);
+        }
+
+        for (char c: t.toCharArray()){
+            if (hm.containsKey(c)){
+                hm.put(c, hm.get(c) -1);
+            }else {
+                return false;
+            }
+        }
+        for (int i: hm.values()){
+            if (i !=0) return false;
+        }
+        return true;
+    }
+
+    public boolean CountSort(String s, String t) {
+        int[] count = new int[26];
+
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
+        }
+
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
