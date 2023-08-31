f0b0e00130a84dcb0a47fc84495df1a244a9ce36
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-08-31 23:10:48 +0530

    Day-4-two-sum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,5 +1,14 @@
 import java.util.*;
-// LINK: https://leetcode.com/problems/two-sum/
+/*
+LINK: https://leetcode.com/problems/two-sum/
+THEORY:
+    1-Create a hashMap
+    2-Iterate through the number array
+    3-Before adding the number to the HashMap
+      Check if target - number is present in the HashMap
+      If its present then the result is the number and target-number
+    4-Else add it to the hashMap
+ */
 
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
@@ -15,3 +24,11 @@ public class TwoSum {
         return new int[]{0,0};
     }
 }
+
+/*
+NEW SYNTAX:
+    1-To create a hashMap: HashMap<Integer, Integer> hm = new HashMap<>()
+    2-To check if it contains a key: hm.containsKey(key)
+    3-To get a value from hm through key: hm.get(key)
+    4-To add a key-value pair: hm.put(key, value)
+ */
