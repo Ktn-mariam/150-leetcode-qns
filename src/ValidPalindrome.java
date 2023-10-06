c139fab45c9fc349afc07868b55d1492ef0ebf3e
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-10-06 19:39:26 +0400

    Day-13-valid-palindrome

--- src/ValidPalindrome.java
+++ src/ValidPalindrome.java
@@ -1,3 +1,15 @@
+/*
+Link: https://leetcode.com/problems/valid-palindrome/
+Theory:
+    1. Initialise left and right pointers for the string
+    2. Run a while for till left < right
+    3. If char at left is not a alphabet, increment left
+    4. If char at right is not a alphabet, decrement right
+    5. else if they are not the same after lowering the case, return false
+    6. Else increment left and decrement right
+    7. Return true on leaving the while loop
+ */
+
 public class ValidPalindrome {
     public boolean isPalindrome(String s) {
         int left = 0;
@@ -22,5 +34,10 @@ public class ValidPalindrome {
 
         return true;
     }
-
 }
+
+/*
+New Syntax learnt:
+    1. Character.isLetterOrDigit(char c): to check if char is a number or alphabet
+    2. Character.toLowerCase(char c): to lowerCase a char
+ */
