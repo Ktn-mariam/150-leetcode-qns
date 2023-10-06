27114b10ba8ab9b155c7178fafae1e24eb7c8b7a
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-10-06 19:33:43 +0400

    Day-13-valid-palindrome

---
+++ src/ValidPalindrome.java
@@ -0,0 +1,26 @@
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                left++;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                right--;
+            } else {
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+
+        return true;
+    }
+
+}
