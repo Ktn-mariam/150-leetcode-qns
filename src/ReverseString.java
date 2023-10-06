ebc3eab2954bc2656b964a7bcaa173a7f1c246da
Mariam Khatoon <mariamkhatoon996@gmail.com>
2023-10-06 22:20:18 +0400

    Day-13-Reverse-String

---
+++ src/ReverseString.java
@@ -0,0 +1,16 @@
+/*
+Link: https://leetcode.com/problems/reverse-string/
+ */
+
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length - 1;
+        while(l <= r) {
+            char tmp = s[l];
+            s[l++] = s[r];
+            s[r--] = tmp;
+        }
+
+    }
+}
